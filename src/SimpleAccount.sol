ed9d765757f1b85546d69dc84f0eae8a8628d232
0xEunum <0xEunum@gmail.com>
2025-12-30 23:18:35 +0530

    feat(SimpleAccount): add smart wallet contract

---
+++ src/SimpleAccount.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+// Layout of Contract:
+// version
+// imports
+// interfaces, libraries, contracts
+// Type declarations
+// errors
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+contract SimpleAccount {
+// ERRORS
+
+// STATE VARIABLES
+
+// EVENTS
+
+// MODIFIERS
+
+// FUNCTIONS
+
+// EXTERNAL FUNCTIONS
+
+// PUBLIC FUNCTIONS
+
+// INTERNAL FUNCTIONS
+
+// PRIVATE FUNCTIONS
+
+// VIEW AND PURE FUNCTION
+}
